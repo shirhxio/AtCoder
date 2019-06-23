fc46e7e15e51f03f6b615bef61cfccad79b5f261
shirhxio <shirhxio537@gmail.com>
2019-06-24 01:05:17 +0900

    [AtCoder] ABC125 B問題追加

---
+++ PastContests/ABC120/ABC125/BResale/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+// https://atcoder.jp/contests/abc125/tasks/abc125_b
+namespace BResale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 入力の取得
+            var n = int.Parse(Console.ReadLine());
+            var values = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var costs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var profits = values.Zip(costs, (v, c) => v - c).ToArray();
+            var maxProfit = profits.Where(x => x > 0).Sum();
+
+            // 解答の取得
+            Console.WriteLine(maxProfit);
+        }
+    }
+}
