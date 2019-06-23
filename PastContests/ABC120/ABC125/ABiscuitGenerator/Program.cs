2d8ff15cbb011cd9eea5fb41b74caea273c4d4f0
shirhxio <shirhxio537@gmail.com>
2019-06-24 00:55:31 +0900

    [AtCoder] ABC125 を追加

---
+++ PastContests/ABC120/ABC125/ABiscuitGenerator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+// https://atcoder.jp/contests/abc125/tasks/abc125_a
+namespace ABiscuitGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 入力の取得
+            var inputs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var a = inputs[0];
+            var b = inputs[1];
+            var t = inputs[2];
+
+            var count = b * (t / a);
+
+            // 解答の出力
+            Console.WriteLine(count);
+        }
+    }
+}
