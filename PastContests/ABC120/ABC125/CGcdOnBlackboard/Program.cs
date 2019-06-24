190d840d2367ce5e997e20ebe3f4fa282b915ff6
shirhxio <shirhxio537@gmail.com>
2019-06-25 01:42:35 +0900

    [AtCoder] ABC125 C問題 愚直解答（たぶんTLE）

---
+++ PastContests/ABC120/ABC125/CGcdOnBlackboard/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace CGcdOnBlackboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 入力の取得
+            var n = int.Parse(Console.ReadLine());
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            // １つの数字以外の最大公約数を全て計算する
+            var gcdList = new int[n];
+            for (var i = 0; i < n; i++)
+            {
+                gcdList[i] = numbers
+                    .Where((_, idx) => idx != i)
+                    .Distinct()
+                    .Aggregate((gcd, num) => Gcd(num, gcd));
+            }
+
+            var answer = gcdList.Max();
+
+            // 解答の出力
+            Console.WriteLine(answer);
+        }
+
+        static int Gcd(int a, int b)
+        {
+            if (a < b) return Gcd(b, a);
+            if (0 < b) return Gcd(b, a % b);
+            return a;
+        }
+    }
+}
