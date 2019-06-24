8a5ec19e7fdc17aaa67955e93730f31a2da117c5
shirhxio <shirhxio537@gmail.com>
2019-06-25 02:23:09 +0900

    [AtCoder] ABC125 C問題解答(対象をUniqueな数字に限定)

--- PastContests/ABC120/ABC125/CGcdOnBlackboard/Program.cs
+++ PastContests/ABC120/ABC125/CGcdOnBlackboard/Program.cs
@@ -9,22 +9,31 @@ namespace CGcdOnBlackboard
         {
             // 入力の取得
             var n = int.Parse(Console.ReadLine());
-            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var inputNums = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
-            // １つの数字以外の最大公約数を全て計算する
-            var gcdList = new int[n];
-            for (var i = 0; i < n; i++)
+            // 1つの数字を書き換えて、最大公約数が変わり得るのはUNIQUEな数字のみ
+            // UNIQUEな数字に対して、その他の数字の最大公約数を計算して、そのうち最大のものが解答
+            var numbersWithCount = inputNums.GroupBy(x => x).Select(g => Tuple.Create(g.Key, g.Count())).ToArray();
+            var numbers = numbersWithCount.Select(x => x.Item1).ToArray();
+            var uniqueNumbers = numbersWithCount.Where(x => x.Item2 == 1).Select(x => x.Item1).ToArray();
+
+            var maxGcd = 0;
+            if (uniqueNumbers.Any())
             {
-                gcdList[i] = numbers
-                    .Where((_, idx) => idx != i)
-                    .Distinct()
-                    .Aggregate((gcd, num) => Gcd(num, gcd));
+                foreach (var uniqueNum in uniqueNumbers)
+                {
+                    var otherGcd = numbers.Where(num => num != uniqueNum)
+                        .Aggregate((gcd, num) => Gcd(num, gcd));
+                    if (maxGcd < otherGcd) maxGcd = otherGcd;
+                }
+            }
+            else
+            {
+                maxGcd = numbers.Aggregate((gcd, num) => Gcd(num, gcd));
             }
 
-            var answer = gcdList.Max();
-
             // 解答の出力
-            Console.WriteLine(answer);
+            Console.WriteLine(maxGcd);
         }
 
         static int Gcd(int a, int b)
