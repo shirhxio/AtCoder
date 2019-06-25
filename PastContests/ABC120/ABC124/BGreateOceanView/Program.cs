8ff4b91f2a3ad97d42d24a9e1d02d9a7250e5d31
shirhxio <shirhxio537@gmail.com>
2019-06-26 04:06:19 +0900

    [AtCoder] ABC124 B問題追加

---
+++ PastContests/ABC120/ABC124/BGreateOceanView/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+// https://atcoder.jp/contests/abc124/tasks/abc124_b
+namespace BGreateOceanView
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 入力の取得
+            var n = int.Parse(Console.ReadLine());
+            var heights = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int maxHeight = 0, oceanViews = 0;
+            foreach (var height in heights)
+            {
+                if (maxHeight <= height)
+                {
+                    maxHeight = height;
+                    oceanViews++;
+                }
+            }
+
+            // 解答の出力
+            Console.WriteLine(oceanViews);
+        }
+    }
+}
